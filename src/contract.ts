a8d585e42df84ce80382c44435e219cf5ec3c827
Iliyan Bachiyski <iliyanbachiyski@gmail.com>
2024-11-14 11:04:13 +0200

    Replace milliseconds with nanoseconds, UnorderedMap with LookupMap, fix prices state format, add validation for creating subscription

--- src/contract.ts
+++ src/contract.ts
@@ -1,9 +1,17 @@
 // Find all our documentation at https://docs.near.org
-import { NearBindgen, UnorderedMap, call, initialize, view } from "near-sdk-js";
+import {
+  NearBindgen,
+  LookupMap,
+  call,
+  initialize,
+  view,
+  assert,
+  near,
+} from "near-sdk-js";
 
 enum PaymentDuration {
-  monthly = 30 * 24 * 60 * 60 * 1000, // 30 days in milliseconds
-  yearly = 365 * 24 * 60 * 60 * 1000, // 365 days in milliseconds
+  monthly = 30 * 24 * 60 * 60 * 1000 * 1000 * 1000, // 30 days in nanoseconds
+  yearly = 365 * 24 * 60 * 60 * 1000 * 1000 * 1000, // 365 days in nanoseconds
 }
 
 enum Plan {
@@ -21,11 +29,16 @@ enum SubscriptionStatus {
   inactive = 2,
 }
 
+type AddSubscriptionRequest = {
+  plan: Plan;
+  paymentDuration: PaymentDuration;
+};
+
 type SubscriptionType = {
   plan: Plan;
   paymentDuration: PaymentDuration;
-  lastPayment: "u64"; // Unix timestamp in milliseconds
-  nextPayment: "u64"; // Unix timestamp in milliseconds
+  lastPayment: bigint; // Unix timestamp in nanoseconds
+  nextPayment: bigint; // Unix timestamp in nanoseconds
   status: SubscriptionStatus;
 };
 
@@ -33,32 +46,29 @@ type SubscriptionType = {
 class Subscription {
   static schema = {
     subscriptions: {
-      class: UnorderedMap,
+      class: LookupMap,
       value: {
         plan: "u8",
-        paymentDuration: "u64",
-        lastPayment: "u64",
-        nextPayment: "u64",
+        paymentDuration: BigInt,
+        lastPayment: BigInt,
+        nextPayment: BigInt,
         status: "u8",
       },
     },
     supported_tokens: "string[]",
     prices: {
-      class: UnorderedMap,
+      class: LookupMap,
       value: {
-        [Plan.basic]: {
-          amount: "u64",
-          token: "string",
-        },
-        [Plan.standard]: { amount: "u64", token: "string" },
-        [Plan.premium]: { amount: "u64", token: "string" },
+        amount: "u64",
+        token: "string",
       },
     },
   };
 
-  subscriptions: UnorderedMap<SubscriptionType> =
-    new UnorderedMap<SubscriptionType>("uid-1");
-  prices: UnorderedMap<PriceType> = new UnorderedMap<PriceType>("uid-2");
+  subscriptions: LookupMap<SubscriptionType> = new LookupMap<SubscriptionType>(
+    "uid-1"
+  );
+  prices: LookupMap<PriceType> = new LookupMap<PriceType>("uid-2");
   supported_tokens: string[] = [];
 
   @initialize({ privateFunction: true })
@@ -66,7 +76,7 @@ class Subscription {
     prices,
     supported_tokens,
   }: {
-    prices: UnorderedMap<PriceType>;
+    prices: LookupMap<PriceType>;
     supported_tokens: string[];
   }) {
     this.supported_tokens = supported_tokens; // ["NEAR", "USDC", "DAI"]
@@ -80,15 +90,47 @@ class Subscription {
   }
 
   @call({ privateFunction: true })
-  update_prices({ value }: { value: UnorderedMap<PriceType> }) {
+  update_prices({ value }: { value: LookupMap<PriceType> }) {
     // TODO: validate params here
     this.prices = value;
   }
 
   @call({})
-  add_subscription({ key, value }: { key: string; value: SubscriptionType }) {
-    // TODO: validate params here
-    this.subscriptions.set(key, value);
+  add_subscription({
+    key,
+    value,
+  }: {
+    key: string;
+    value: AddSubscriptionRequest;
+  }) {
+    const caller = near.predecessorAccountId();
+    // Assert that the caller is the same as the key
+    assert(caller === key, "Can not add subscription for another account");
+    // Assert that the caller is not already in the map
+    assert(
+      !this.subscriptions.containsKey(caller),
+      "Subscription already exists"
+    );
+    const { plan, paymentDuration } = value;
+    // Assert that the plan is valid
+    assert(Object.values(Plan).includes(plan), "Invalid plan");
+    // Assert that the paymentDuration is valid
+    assert(
+      Object.values(PaymentDuration).includes(paymentDuration),
+      "Invalid payment duration"
+    );
+    // Assert that the plan is supported
+    assert(this.prices.containsKey(plan.toString()), "Plan not supported");
+    // TODO: Probably initial payment should be done here
+    const blockTimestamp = near.blockTimestamp();
+    const subscription: SubscriptionType = {
+      plan,
+      paymentDuration,
+      lastPayment: blockTimestamp,
+      nextPayment: blockTimestamp + BigInt(paymentDuration.toString()),
+      status: SubscriptionStatus.active,
+    };
+    this.subscriptions.set(key, subscription);
   }
 
   @call({})
@@ -100,16 +142,5 @@ class Subscription {
   @view({})
   get_account_subscription({ account }: { account: string }): SubscriptionType {
     return this.subscriptions.get(account);
-  }
-
-  @view({})
-  get_subscriptions({
-    from_index,
-    limit,
-  }: {
-    from_index: number;
-    limit: number;
-  }) {
-    return this.subscriptions.toArray().slice(from_index, limit);
   }
 }
